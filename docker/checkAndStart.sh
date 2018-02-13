ee4f1232cd7e9da8b6429ec29aedb8928972cbcc
Uwe Klosa <uwe.klosa@ub.uu.se>
2018-02-13 14:27:34 +0100

    CORA-514: added one authority person record to DiVA's Fedora

---
+++ docker/checkAndStart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+COUNTER=`find fedora38/data -name "info*authority-person*" | wc -l`
+
+if [ $COUNTER -eq 0 ]
+then
+  startup.sh
+  sleep 5
+  ./ingestPeople.sh
+  sleep 1
+  shutdown.sh
+  sleep 5
+fi
+
+catalina.sh run
