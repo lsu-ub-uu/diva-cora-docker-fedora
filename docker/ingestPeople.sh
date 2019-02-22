b7d749bbbeae6efac633e3e329e92e73af2b74b4
olovm <olov.mckie@ub.uu.se>
2019-02-22 13:13:55 +0000

    work on fedora 3.2

--- docker/ingestPeople.sh
+++ docker/ingestPeople.sh
@@ -3,5 +3,5 @@
 echo "Ingesting authority person records"
 for FILE in `find import -type f`
 do
-  ./fedora38/client/bin/fedora-ingest.sh f $FILE info:fedora/fedora-system:ATOMZip-1.1 localhost:8088 fedoraAdmin changeit http
+  ./fedora32/client/bin/fedora-ingest.sh f $FILE info:fedora/fedora-system:ATOMZip-1.1 localhost:8088 fedoraAdmin changeit http
 done
