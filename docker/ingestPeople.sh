c9ebe56797ed1efd4a81225930645346db436854
olov <olov@b1ec04b41c42>
2019-02-28 12:03:53 +0000

    removing ingestPeople as it does not work in fedora 3.2.1

--- docker/ingestPeople.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Ingesting authority person records"
-for FILE in `find import -type f`
-do
-  ./fedora32/client/bin/fedora-ingest.sh f $FILE info:fedora/fedora-system:ATOMZip-1.1 localhost:8088 fedoraAdmin changeit http
-done
